eb3059578d5d8d48ee85c99f8705c80f95e7cde8
Rinat <you@example.com>
2025-08-02 17:58:25 +0500

    fxied favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,12 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href="/favicon.ico" />
+	<link rel="icon" href="/favicon/favicon.ico" />
+	<link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href="favicon/favicon.svg" />
+	<link rel="shortcut icon" href="/favicon/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+	<link rel="manifest" href="/favicon/site.webmanifest" />
 	<style>
 		@font-face {
 			font-family: 'IBM Plex Sans';
